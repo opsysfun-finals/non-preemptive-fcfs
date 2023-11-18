8b3fe8af6d13d5d001d925e37a99167aa31ded3d
Ken Shamrock L. Dizon <dizonshamrock@gmail.com>
2023-11-18 22:36:20 +0800

    Implemnted combining of arrival and burst arrays

--- process.js
+++ process.js
@@ -16,13 +16,34 @@ function processData(form) {
     let y = getCpuCycles(form);
 
     // Convert the string to an array
-    let xArr = convertData(x);
-    let yArr = convertData(y);
+    let arrival_array = convertData(x);
+    let burst_array = convertData(y);
 
     // Check first if the length of the two arrays are equal
-    if (xArr.length !== yArr.length) {
+    if (arrival_array.length !== burst_array.length) {
         alert("Invalid Input. Arrival Times and CPU Cycle count are not the same!")
         return;
     }
+    
+    let pair_array = combineArrays(arrival_array, burst_array);
+}
 
+function combineArrays(arrival_array, burst_array) {
+    let pair_array = [];
+    
+    // Combine the two arrays to form a key value pair
+    for (let i = 0; i < arrival_array.length; i++) {
+        let pair = {};
+        pair["arrival"] = arrival_array[i];
+        pair["burst"] = burst_array[i];
+        pair_array.push(pair);
+    }
+
+    // Sort array
+    pair_array.sort((a,b) => a.arrival - b.arrival);
+    return pair_array;
+}
+
+function solveIdle(pair_array) {
+    
 }
