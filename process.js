d11bce2012303f505149186649aef7d385ce3f6d
Ken Shamrock L. Dizon <dizonshamrock@gmail.com>
2023-11-19 13:57:52 +0800

    Arranged some functions

--- process.js
+++ process.js
@@ -11,23 +11,6 @@ function convertData(string) {
     return array;
 }
 
-function processData(form) {
-    let x = getArrivalTimes(form);
-    let y = getCpuCycles(form);
-
-    // Convert the string to an array
-    let arrival_array = convertData(x);
-    let burst_array = convertData(y);
-
-    // Check first if the length of the two arrays are equal
-    if (arrival_array.length !== burst_array.length) {
-        alert("Invalid Input. Arrival Times and CPU Cycle count are not the same!")
-        return;
-    }
-    
-    let pair_array = combineArrays(arrival_array, burst_array);
-}
-
 function combineArrays(arrival_array, burst_array) {
     let pair_array = [];
     
@@ -44,6 +27,40 @@ function combineArrays(arrival_array, burst_array) {
     return pair_array;
 }
 
-function solveIdle(pair_array) {
+/*
+    Include the idle!
+    pair_array Must already be sorted!
+*/
+function schedule(pair_array) {
+    let clock = 0;
+    let start_times = [];
+    let end_times = [];
+    let waiting_times = [];
+    let turnover_times = [];
+
+    for (let i = 0; i < pair_array.length; i++) {
+        // Check where to start
+        if (i === 0) {
+            start_times.push(pair_array[i].arrival);
+        }
+
+    }
+}
+
+function processData(form) {
+    let x = getArrivalTimes(form);
+    let y = getCpuCycles(form);
+
+    // Convert the string to an array
+    let arrival_array = convertData(x);
+    let burst_array = convertData(y);
+
+    // Check first if the length of the two arrays are equal
+    if (arrival_array.length !== burst_array.length) {
+        alert("Invalid Input. Arrival Times and CPU Cycle count are not the same!")
+        return;
+    }
     
+    // This array does not know if it contains idle
+    let pair_array = combineArrays(arrival_array, burst_array);
 }
